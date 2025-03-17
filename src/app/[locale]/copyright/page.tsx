e265c00ae7f661c54477e06db51958b13fcce9f3
manu9898 <manumuscat3@gmail.com>
2025-03-17 13:09:17 +0100

    ajout des sauts de lignes dans la page copyright

--- src/app/[locale]/copyright/page.tsx
+++ src/app/[locale]/copyright/page.tsx
@@ -9,7 +9,7 @@ export default function Copyright() {
     <div className="container">
       <p className="py-20">1. Présentation du site.
       En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site https://julien-architecte.vercel.app/fr l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
-<br /><br />
+      <br /><br />
       Propriétaire : Julien Blatteau – Siret – Adresse – 13001 Marseille
       Créateur : Emmanuel Muscat,
       Responsable publication : Emmanuel Muscat
